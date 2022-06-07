669fa56cced6844ad52727b5ff3bd92abd0da6b1
Venkata Nagendra Babu Yanamadala <venkata.yanamadala@dnata.com>
2022-06-07 11:46:21 +0400

    Initial Project setup with required dependencies

---
+++ src/main/java/com/movieportal/booking/BookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movieportal.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieportal/booking/BookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieportal.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
